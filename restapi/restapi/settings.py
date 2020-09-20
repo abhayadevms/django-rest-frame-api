715eced796a55322c6feddc243942e04948ee03e
Abhayadev ms <mail.abhayadev@gmail.com>
2020-09-20 15:58:14 +0530

    Update settings.py

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3p@eett9en3_!b)80a-ra6(t92=n6!u3e=-1vs3+wr$506r_#v'
+SECRET_KEY = 'put your secrtekey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
